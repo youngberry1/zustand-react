a37c1e8793adc49137f90fd7af0628c4918591b4
youngberry1 <abdulabdul28@aol.com>
2025-10-04 10:32:56 +0000

    deleted .env

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const base = import.meta.env.VITE_DEPLOY_ENV === "GH_PAGES" ? "/zustand-react/" : "/";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base
+  base: "/zustand-react/"
 })
